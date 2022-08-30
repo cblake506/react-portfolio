d37e519d301db72504594e21d96397ec02181421
Connor Blake <cblake506@gmail.com>
2022-08-30 17:39:48 -0500

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <footer class="container-fluid bg-4 text-center">
+        <p>
+            <a href="mailto:cblake506@gmail.com">cblake506@gmail.com</a> |
+            <a href="https://www.linkedin.com/in/connor-blake-b27161157/" target="_blank">LinkedIn</a> | 
+            <a href="assets/Resume-Connor-Blake.pdf" download="" target="_blank"> Resume</a>
+        </p> 
+        </footer>
+    );
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Header () {
+    return (
+        <nav class="navbar navbar-default" id="home">
+            {/* <div class="container">
+                <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="#home">Connor Blake</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#who">WHO</a></li>
+                    <li><a href="#what">WHAT</a></li>
+                </ul>
+                </div>
+            </div> */}
+        </nav>
+    );
+}
+export default Header;
