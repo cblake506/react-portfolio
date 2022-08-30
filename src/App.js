edabc6a4577d8d49707cc3e584eaa498e95a5553
Connor Blake <cblake506@gmail.com>
2022-08-30 18:03:26 -0500

    added about

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('Home');
   return (
     <div className="App">
       <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <About />
       <Footer />
     </div>
   );
